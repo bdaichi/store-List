dd1209d3e3ebbe92f2915988013bef35af9b5289
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-03-08 16:05:20 +0900

    apikeyを変更した

--- service/firebase_service.ts
+++ service/firebase_service.ts
@@ -14,7 +14,7 @@ try {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     };
     if (!firebase.apps.length) {
         firebase.initializeApp(config);
